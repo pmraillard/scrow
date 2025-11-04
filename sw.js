0ceb456f33563d50b51b62f28c4413cc9b20a3b1
pmraillard <104978585+pmraillard@users.noreply.github.com>
2025-11-04 17:13:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,3 @@
-const CACHE_NAME = 'nfc-writer-proto-v1';
-const FILES_TO_CACHE = [
-  '/index.html',
-  '/manifest.json'
-];
-// Basic service worker to make the prototype installable (offline shell).
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((resp) => {
-      return resp || fetch(evt.request);
-    })
-  );
-});
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => self.clients.claim());
+self.addEventListener("fetch", () => {});
